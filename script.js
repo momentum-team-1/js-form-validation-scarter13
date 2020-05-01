f02e84f7f1fd41104ff87d05f068db136e1446a5
scarter13 <scarter13@gmail.com>
2020-05-01 13:05:12 -0400

    first js working

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+
+// let form = document.querySelector(#parking-form)
 console.log('Add validation!');
+
+// set up variable for one field
+//set up variable to grab content of that field
+// create function to console log value of variable
+// set up variables for all fields
+
+let nameinput = document.querySelector(#name);
+let givenName = nameinput.value;
+
+function 
