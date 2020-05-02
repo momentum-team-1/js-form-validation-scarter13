95abee5f1cc3b327af32b95a934ef32f18e1828e
scarter13 <scarter13@gmail.com>
2020-05-02 13:19:07 -0400

    first three working, now trying to combine car function

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ form.addEventListener('submit', function(event){
     console.log ("button was clicked")
 
     validateName()
-    // validateCarYear ()
-    // validateCarMake ()
+    validateCarYear ()
+    validateCarMake()
     // validateCarModel ()
     // validateDate ()
     // validateDuration ()
@@ -20,6 +20,8 @@ form.addEventListener('submit', function(event){
     // validateExpiry ()
    
 })
+
+// the variable assignments for 'something -label' are, for now, placeholders to create text on the appropriate fields if that field is evaluated as invalid.  The variable definitions are not commented out, but their assignments are.
 
 function validateName() {
 let nameInput = document.querySelector('#name');
@@ -38,8 +40,46 @@ if (nameValue !== ""){
     }
 }
 
+function validateCarYear() {
+    let carYearInput = document.querySelector('#car-year');
+    let carYearValue = carYearInput.value;
+    console.log (carYearValue)
+    let inputGroupField = carYearInput.parentElement
+    let carYearLabel = document.querySelector("car-year-label")
+    if (carYearValue !== ""){
+        inputGroupField.classList.remove("input-invalid");
+        inputGroupField.classList.add("input-valid");
+    // carYearLabel.textContent="Car Year";
+    } else {
+        inputGroupField.classList.remove("input-valid");
+        inputGroupField.classList.add("input-invalid");
+    // carYearLabel.textContent="Car Year is Required"
+        }
+    }
+
+    function validateCarMake() {
+    let carMakeInput = document.querySelector('#car-make');
+    let carMakeValue = carMakeInput.value;
+    console.log (carMakeValue)
+    let inputGroupField = carMakeInput.parentElement
+    let carMakeLabel = document.querySelector("car-make-label")
+    if (carMakeValue !== ""){
+        inputGroupField.classList.remove("input-invalid");
+        inputGroupField.classList.add("input-valid");
+    // carMakeLabel.textContent="Car Make";
+    } else {
+        inputGroupField.classList.remove("input-valid");
+        inputGroupField.classList.add("input-invalid");
+    // carmakeLabel.textContent="Car Make is Required"
+        }
+    }
+
+
     
 
+
+// yourVariableLabel.textContent="thing";
+// yourVariableLabel.textContent="thing is Required"
 
 
 // set up variable for one field
